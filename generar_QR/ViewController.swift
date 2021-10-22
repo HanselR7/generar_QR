497eb5a4cb7ea246adf340612949fb320454cb3b
HanselRamoscorp <hansel.ramoscorp@outlook.com>
2021-10-22 14:35:49 -0500

    Initial Commit

---
+++ generar_QR/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  generar_QR
+//
+//  Created by Hansel on 22/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
