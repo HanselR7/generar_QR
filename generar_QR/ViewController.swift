74d384155c164e2dc703ee9b72da059a3917f678
HanselRamoscorp <hansel.ramoscorp@outlook.com>
2021-10-22 15:53:49 -0500

    Generar qr

--- generar_QR/ViewController.swift
+++ generar_QR/ViewController.swift
@@ -1,19 +1,28 @@
-//
-//  ViewController.swift
-//  generar_QR
-//
-//  Created by Hansel on 22/10/21.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    //Variables de interfaz
+    @IBOutlet weak var imgQR: UIImageView!
+    
+    //Variables de trabajo
+    var mensaje = "Hola como estas?"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        imgQR.image = genera_qr(texto: mensaje)
     }
 
-
+    func genera_qr(texto: String) -> UIImage? {
+        let data = texto.data(using: String.Encoding.ascii)
+        if let filter = CIFilter(name: "CIQRCodeGenerator") {
+            filter.setValue(data, forKey: "InputMessage")
+            let transform = CGAffineTransform(scaleX: 5, y: 5)
+            if let salida = filter.outputImage?.transformed(by: transform) {
+                return UIImage(ciImage: salida)
+            }
+        }
+        return nil
+    }
+    
 }
 
